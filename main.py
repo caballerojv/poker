2d5ea474d1923caacbb693a3d4b7bcc14fb5fec1
caballerojv <58597283+caballerojv@users.noreply.github.com>
2023-05-27 17:26:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,23 @@ import roles
 
 class Game:
     def __init__(self, num_players):
-        self.num_players = num_players
+        self.players = []
+        for i in range(1, num_players + 1):
+            player = roles.Player()
+            player.name = f"Player {i}"
+            self.players.append(player)
+            # acabar main segun vayamos avanzando
+        pass
+
+    def start_game(self):
+        self.dealer = roles.Dealer()
+        self.deck = self.dealer.deck = cards.Deck()
+        self.dealer.deck.shuffle_deck()
+        self.dealer.get_common_cards()
+        for player in self.players:
+            player.cards = self.dealer.give_personal_cards()
+
+
+game1 = Game(7)
+print(game1.start_game())
+print([player.name for player in game1.players])
