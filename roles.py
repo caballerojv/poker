e6c005c801d2a881b6f7e574b1961f070fafa0f9
caballerojv <58597283+caballerojv@users.noreply.github.com>
2023-05-27 17:26:52 +0100

    Update roles.py

--- roles.py
+++ roles.py
@@ -4,22 +4,18 @@ import cards
 
 class Dealer:
     def __init__(self) -> None:
-        table = []
-        for _ in range(5):
-            table.append(cards.Deck().give_top_card(self))
-        self.table = table
+        pass
 
-        cards_for_player = []
-        for _ in range(2):
-            cards_for_player.append(cards.Deck().give_top_card(self))
-        self.cards_for_player = cards_for_player
+    def get_common_cards(self):
+        return [self.deck.give_top_card() for _ in range(5)]
+
+    def give_personal_cards(self):
+        return [self.deck.give_top_card() for _ in range(2)]
+
+    def __str__(self):
+        return "".join(f"{card.value}{card.suit}" for card in self.cards_for_player)
 
 
 class Player:
-    def __init__(self) -> None:
+    def __init__(self) -> None:  # acabar player
         pass
-
-
-dealer1 = Dealer()
-print(dealer1.cards_for_player)
-print(dealer1.table)
